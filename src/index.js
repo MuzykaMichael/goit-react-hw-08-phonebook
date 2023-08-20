cadb344582957f945a58eeb573997eae083151fe
Muzyka Mykhailo <Muzyka_Mishel@ukr.net>
2023-08-20 17:23:26 +0300

    no unused vars

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Outlet } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { App } from 'components/App';
 import './index.css';
 import { Provider } from 'react-redux';
